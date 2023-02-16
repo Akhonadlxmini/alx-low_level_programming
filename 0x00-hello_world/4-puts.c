9cdd276a960220de3a0e107bd8d77e694eeb70c0
Akhonadlxmini <yaandidlaminii@gmail.com>
2023-02-16 12:34:38 -0800

    4putsc

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
  * Return: Always 0 (Success)
@@ -6,6 +7,6 @@
 
 int main(void)
 {
-	puts("\"Programing is like building a multilingual puzzle");
-	        return (0);
+	puts("Programing is like building a multilingual puzzle\n");
+	return (0);
 }
